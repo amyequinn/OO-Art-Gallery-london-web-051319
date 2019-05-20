a960be45c42b4316ec26510d6fe99005d4ff2c95
Amy Quinn <amyequinn@hotmail.co.uk>
2019-05-20 22:14:28 +0100

    art gallery

--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,17 @@
 require_relative '../config/environment.rb'
+require 'pry'
+
+artist1 = Artist.new("Amy",2)
+artist2 = Artist.new("John",3)
+artist3 = Artist.new("Paul",4)
+
+gallery1= Gallery.new("GOne", "London")
+gallery2= Gallery.new("GTwo", "London")
+gallery3= Gallery.new("GThree", "Dublin")
+
+painting1=Painting.new("Pone", "£10")
+painting2=Painting.new("PTwo", "£20")
+painting3=Painting.new("PThree", "£30")
 
 
-
-
-binding.pry
-
-puts "Bob Ross rules."
+Pry.start
